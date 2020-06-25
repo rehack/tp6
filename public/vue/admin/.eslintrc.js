e507fc9290e0fb70ebf5a9b578693d0e908a295b
codesboy <565195693@qq.com>
2020-06-25 18:18:12 +0800

    update

---
+++ public/vue/admin/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "root": true,
+    "env": {
+      "node": true,
+      "es6": true
+    },
+    "extends": [
+      "plugin:vue/essential",
+      "@vue/standard"
+    ],
+    "parserOptions": {
+      "parser": "babel-eslint"
+    },
+    "rules": {
+      "indent": [2, 4]
+    }
+};
--- public/vue/admin/src/views/Home.vue
+++ public/vue/admin/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
     <button @click="testApi">按钮</button>
   </div>
 </template>
@@ -12,21 +12,23 @@ import axios from 'axios'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  },
+    name: 'Home',
+    components: {
+        HelloWorld
+    },
 
-  methods: {
-    testApi () {
-      axios.post('http://tp6.com/api/test', {
-        headers: {
-          Authorization: 'rehack34543ca7ce6357e2422071984d94b962'
+    methods: {
+        testApi () {
+            axios
+                .post('http://tp6.com/api/test', {
+                    headers: {
+                        Authorization: 'rehack34543ca7ce6357e2422071984d94b962'
+                    }
+                })
+                .then(res => {
+                    console.log(res)
+                })
         }
-      }).then(res => {
-        console.log(res)
-      })
     }
-  }
 }
 </script>
